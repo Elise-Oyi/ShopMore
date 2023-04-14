54f1fba5f989672c1df01aa5bb7396ee4afef582
Elise-oyi <maryeliseangbo@gmail.com>
2023-04-14 22:40:55 +0100

    Add project files.

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,93 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(StoreContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+               new Product
+                {
+                    Name = "Angular Speedster Board 2000",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 20000,
+                    PictureUrl = "/images/products/sb-ang1.png",
+                    Brand = "Angular",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Green Angular Board 3000",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/sb-ang2.png",
+                    Brand = "Angular",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Core Board Speed Rush 3",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/sb-core1.png",
+                    Brand = "NetCore",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Net Core Super Board",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 30000,
+                    PictureUrl = "/images/products/sb-core2.png",
+                    Brand = "NetCore",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "React Board Super Whizzy Fast",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/sb-react1.png",
+                    Brand = "React",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Typescript Entry Board",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 12000,
+                    PictureUrl = "/images/products/sb-ts1.png",
+                    Brand = "TypeScript",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                }
+
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
